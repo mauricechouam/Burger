78c4417ef7c49d5bf15ec8e34b959a6b33fdd29e
mauricechouam <lansichouamou@gmail.com>
2020-07-02 02:08:19 -0700

    added put'

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,12 +2,13 @@
 var express = require("express");
 // import the model to use its db functions for burger.js
 var burger = require("../models/burger.js");
+const { realpathSync } = require("fs");
 
 // create the router for the app, and export 
 var router = express.Router();
 // Create routes and set up logic where required.
 router.get("/", function (req, res) {
-    burger.selectAll(function(data) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -22,5 +23,19 @@ router.post("/api/burgers", function (req, res) {
         // send back the ID of the new burger
         res.json({ id: result.insertId });
     });
+});
 
+// set burger devoured status to true
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "idn " + req.params.id;
+    console.log("condition", condition);
+    burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
+        if (result.changedRows === 0) {
+            // if no rows were changed, then the ID must not exit, so 404
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
 });
