dcbc71e213fb1ae4d12b5265ba99f79e50c60a08
mauricechouam <lansichouamou@gmail.com>
2020-07-02 01:59:16 -0700

    added get and post

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,3 +4,23 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 // create the router for the app, and export 
+var router = express.Router();
+// Create routes and set up logic where required.
+router.get("/", function (req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+//add new Burger to the db
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+        // send back the ID of the new burger
+        res.json({ id: result.insertId });
+    });
+
+});
