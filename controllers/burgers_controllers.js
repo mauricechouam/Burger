1568a96caf203fb605cc06dbb087ccab36f59c7e
mauricechouam <lansichouamou@gmail.com>
2020-07-02 01:45:12 -0700

    added Started burger_controller.js code by adding dependencies and import the model

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,6 @@
+// dependencies
+var express = require("express");
+// import the model to use its db functions for burger.js
+var burger = require("../models/burger.js");
+
+// create the router for the app, and export 
